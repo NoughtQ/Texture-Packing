d52600e96e6b8fe9a68fbd87ce6aedc6ae82e811
au12321ua <12321ua@gmail.com>
2024-11-27 21:32:46 +0800

    生成函数v1.2

--- generate.cpp
+++ generate.cpp
@@ -5,29 +5,41 @@
 #include<random>
 using namespace std;
 
-typedef pair<int,int> rectangle;
+typedef pair<double,double> rectangle;
 
-vector<rectangle> generate(int maxHeight,int maxWeight,int num, int mode);
-void printrecs(vector<rectangle> recs, int maxHeight, int maxWeight, string filename);
+vector<rectangle> generate(double maxHeight,double maxWeight,int num, int mode);
+void printrecs(vector<rectangle> recs, double maxHeight, double maxWeight, string filename);
 
 
 int main()
 {
-    //mode 0: uniform int distribution
-    //mode 1：normal distribution
-    int maxHeight,maxWeight,num,mode;
-    string filename;
-    cin >> maxHeight >> maxWeight >> num >> mode;
-    cin >> filename;
+    fstream infile;
+    infile.open("generate_in.txt", ios::in);
     
-    vector<rectangle> recs;
-    recs = generate(maxHeight, maxWeight, num, mode);
-    printrecs(recs, maxHeight, maxWeight, filename);
+    //mode 0: uniform int distribution
+    //mode 1：normal distribution(int)
+    //mode 2: uniform real distribution
+    //mode 3: normal distribution(double)
+    int i;
+    double maxHeight,maxWeight;
+    int num,mode,n;
+    string filename;
+
+    infile >> n;
+    for(i=0; i<n; i++)
+    {
+        infile >> maxHeight >> maxWeight >> num >> mode;
+        infile >> filename;
+        
+        vector<rectangle> recs;
+        recs = generate(maxHeight, maxWeight, num, mode);
+        printrecs(recs, maxHeight, maxWeight, filename); 
+    }
 
     return 0;
 }
 
-vector<rectangle> generate(int maxHeight,int maxWeight,int num, int mode)
+vector<rectangle> generate(double maxHeight,double maxWeight,int num, int mode)
 {
     vector<rectangle> recs;
     random_device rd;
@@ -62,18 +74,51 @@ vector<rectangle> generate(int maxHeight,int maxWeight,int num, int mode)
             recs.push_back(make_pair(h,w));
         }
     }
+    else if(mode == 2)
+    {
+        uniform_real_distribution<> height(1, maxHeight);
+        uniform_real_distribution<> weight(1, maxWeight);
+        for(int i=0; i<num; i++)
+            recs.push_back(make_pair(height(gen),weight(gen)));
+    }
+    else if(mode == 3)
+    {
+        normal_distribution<> height(static_cast <double> (maxHeight/2), static_cast <double> (maxHeight/6));
+        normal_distribution<> weight(static_cast <double> (maxWeight/2), static_cast <double> (maxWeight/6));
+        for(int i=0; i<num; i++)
+        {
+            double h,w;
+            h = height(gen);
+            w = weight(gen);
+
+            if(h <= 0) 
+                h = 0.1;
+            if(w <= 0)
+                w = 0.1;
+            
+            recs.push_back(make_pair(h,w));
+        }
+    }
 
     return recs;
 }
 
-void printrecs(vector<rectangle> recs, int maxHeight, int maxWeight, string filename)
+void printrecs(vector<rectangle> recs, double maxHeight, double maxWeight, string filename)
 {
     fstream outfile;
     outfile.open("data/"+filename, ios::out);
 
-    outfile << maxHeight << " " << maxWeight << " " << recs.size() << std::endl;
+    /*********************
+     * test example:
+     * maxWeight
+     * n 
+     * weight_1 height_1
+     * ...
+     * weight_n height_n
+    **********************/
+    outfile << maxWeight << " " << recs.size() << std::endl;
     for(int i=0; i<recs.size(); i++)
-        outfile << recs[i].first << " " << recs[i].second << std::endl;
+        outfile << recs[i].second << " " << recs[i].first << std::endl;
     
     outfile.close();
 }
