d2f451bd6d956d278bec529657a6fe2dc7041fc7
NoughtQ <noughtq666@gmail.com>
2024-11-29 08:56:15 +0800

    Add Case 3 for correctness test

--- code/scripts/ttpHeader.h
+++ code/scripts/ttpHeader.h
@@ -1,7 +1,7 @@
 #define ITEMNUM 1000000                           // Maximum number of items
 #define ITERATIONS 1                             // Iteration time
-#define INPUTDIR "../test/uniform_double_160000"        // Directory of the input file
-#define OUTPUTDIR "../test/FFDH/output2"                   // Directory of the output file
+#define INPUTDIR "../test/FFDH/input3"        // Directory of the input file
+#define OUTPUTDIR "../test/FFDH/output3"                   // Directory of the output file
 
 // structure of a single item
 typedef struct item {
